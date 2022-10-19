db6d27165788334d9541a7eacee440beb00fdc98
mustaq712 <110221049+mustaq712@users.noreply.github.com>
2022-10-19 16:33:34 +0530

    Added files fo the contacts React App

---
+++ src/contacts.js
@@ -0,0 +1,38 @@
+const contacts = [
+  {
+    id: 1,
+    name: "Michael Scott",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/d/dc/MichaelScott.png/220px-MichaelScott.png",
+    phone: "7894 143 143",
+    email: "littlekidslover@gmail.com"
+  },
+  {
+    id: 2,
+    name: "Phil Dunphy",
+    imgURL:
+    "https://pbs.twimg.com/profile_images/2711772111/5579fa8742299df69b02af2c7a2a9c51_400x400.png",
+    phone: "9987 654 321",
+    email: "philrrealtor@gmail.com"
+  },
+  {
+    id: 3,
+    name: "Barney Stinson",
+    imgURL:
+      "https://openpsychometrics.org/tests/characters/test-resources/pics/HIMYM/4.jpg",
+    phone: "8383 699 699",
+    email: "legendarystinson@gmail.com"
+  },
+  {
+    id : 4,
+    name : "Jake Peralta",
+    imgURL:
+       "https://pbs.twimg.com/profile_images/928266336073117696/W2gCidjA_400x400.jpg" ,
+    phone : "7878 345 134",
+    email : "cooljake@yahoo.com"       
+  }
+
+
+];
+
+export default contacts;
